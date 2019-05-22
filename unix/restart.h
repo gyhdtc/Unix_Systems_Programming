153766ea9346e83deccb83f17d95ddf4ba33c478
Your Name <gyh108wy@163.com>
2019-05-23 00:40:42 +0800

    1

--- unix/restart.h
+++ unix/restart.h
@@ -2,6 +2,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+pid_t r_wait(int *stat_loc) {
+    int retval;
+    while (((retval = wait(stat_loc)) == -1) && (errno == EINTR));
+    return retval;
+}
 
 int r_close(int r_fildes)
 {
