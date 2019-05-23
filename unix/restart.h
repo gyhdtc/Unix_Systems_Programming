ae899f73cd6b3a8eeb6a696a5e30501bab4ffb72
gyhdtc <gyh108wy@163.com>
2019-05-23 12:27:47 +0800

    1

--- unix/restart.h
+++ unix/restart.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
 pid_t r_wait(int *stat_loc) {
     int retval;
     while (((retval = wait(stat_loc)) == -1) && (errno == EINTR));
